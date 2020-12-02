0224f43f605fe407d607391f08a65cdfd3cc4311
sew9309 <70584532+sew9309@users.noreply.github.com>
2020-12-02 12:52:34 -0500

    scene manager works!!

--- Final Sketch + Index Folder/sketch.js
+++ Final Sketch + Index Folder/sketch.js
@@ -1,23 +1,81 @@
+let screen = 0;
+let mgr;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(100);
-}
+
+	mgr = new SceneManager();
+
+	// Preload scenes. Preloading is normally optional
+    // ... but needed if showNextScene() is used.
+    mgr.addScene ( Intro );
+    mgr.addScene ( Game );
+  //  mgr.addScene ( Animation3 );
+
+    mgr.showNextScene();
+
+	// for (let i = 0; i < 50; i++) {
+ //    syringes.push(new Syringe());
+  }
+
 
 function draw() {
 	background(0);
+
+	mgr.draw();
 	
-	// Screen 1
-	fill(255);
-	rect(50, 50, 100, 100);
-	rect(150, 50, 100, 100);
-	rect(150, 150, 100, 100);
-	
-	rectMode(CENTER);
+}
+
+function keyPressed()
+{
+    // You can optionaly handle the key press at global level...
+    switch(key)
+    {
+        case '1':
+            mgr.showScene( Intro );
+            break;
+        case '2':
+            mgr.showScene( Game );
+            break;
+      //  case '3':
+       //     mgr.showScene( Animation3 );
+       //     break;
+
+
+    }
+    
+    // ... then dispatch via the SceneManager.
+    mgr.handleEvent("keyPressed");
+}
+
+
+function Intro()
+{
+    this.setup = function() {
+    }
+
+    this.draw = function() {
+    rectMode(CENTER);
 	fill(255);
 	rect(width/2, height/2, 200, 50, 50);
 	fill(0, 255, 0);
 	ellipse(width/2-74, height/2, 50, 50);
-	
-	function mouseDragged(){
-	}
+    }
+
+    this.keyPressed = function() {
+        // switch the scene
+        this.sceneManager.showScene( Game );
+    }
 }
+
+// Main games scene constructor function
+function Game()
+{
+    this.setup = function() {
+    }
+
+    this.draw = function() {
+			background(255,0,0);
+    }
+} 
